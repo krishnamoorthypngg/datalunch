e8b4fef0b15197c1259ee6984e359f86756f1c96
ibrahim199924 <62646119+ibrahim199924@users.noreply.github.com>
2020-03-25 21:29:52 +1100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace helloworld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Message[] msg = new Message[4];
+            msg[0] = new Message("HELP!!!");
+            msg[1] = new Message("Mayday,mayday");
+            msg[2] = new Message("we have casualties");
+            msg[3] = new Message("I am on my way");
+
+            
+            string input = Console.ReadLine().ToLower();
+            if (input == "ben")
+            {
+                msg[0].Print();
+            }
+            else if(input == "timothy")
+                {
+                msg[1].Print();
+            }
+            else if(input == "alex")
+            {
+                msg[2].Print();
+            }
+            else
+            {
+                msg[3].Print();
+            }
+        }
+    }
+}
